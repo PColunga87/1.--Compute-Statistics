71326c9fd9b70797edb90e19f924889d36d25330
PColunga87 <113809304+PColunga87@users.noreply.github.com>
2024-02-18 23:58:09 -0600

    Add files via upload

---
+++ 6.2. Ejercicio de programación 3/Work/room/room.py
@@ -0,0 +1,20 @@
+# pylint: disable=C0103
+# pylint: disable=W0718
+"""Code to execute the class customer"""
+
+class Room:
+    """
+    Represents a room.
+
+    Attributes:
+    - number (int): The room number.
+    """
+
+    def __init__(self, number):
+        """
+        Initialize a Room instance.
+
+        Parameters:
+        - number (int): The room number.
+        """
+        self.number = number
